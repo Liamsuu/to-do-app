b9fa9f8c38b6aec4576f85527597927d602c965b
Liam <liamsuu14974@gmail.com>
2024-04-18 22:50:14 +0100

    Rework file structure for component files

---
+++ components/task/task.js
@@ -0,0 +1,12 @@
+export default class Task {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
+
+export function deleteTaskObj(taskObj) {
+  taskObj = undefined;
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import "./index.css";
-import createProjectBar from "./projectSection/createProjectBar";
+import createProjectBar from "../components/projectSection/createProjectBar";
 import appendToMain from "./appendToMainSection";
-import createTaskSection from "./task/createTaskSection";
+import createTaskSection from "../components/task/createTaskSection";
+import Task from "../components/task/task";
+import { deleteTaskObj } from "../components/task/task";
 
 appendToMain(createProjectBar());
 appendToMain(createTaskSection()); // create todosection and put in here.
+const newTask = new Task();
