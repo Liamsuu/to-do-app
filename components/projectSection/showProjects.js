ea9097eeefec80b12d30c3e7364e40ebd5c01035
Liam <liamsuu14974@gmail.com>
2024-04-21 23:15:20 +0100

    Make small change in css, prepare for reconstruction of tasks functionality

--- components/projectSection/showProjects.js
+++ components/projectSection/showProjects.js
@@ -38,3 +38,13 @@ export default function showProjects() {
     projectBar.appendChild(projectsContainer);
   });
 }
+
+/**Will have to find a way to save the state of the tasks(they're just objects remember).
+ * Likely will have to have a seperate array of Task objects for each project and link
+ * them by the id number of the project and remove that task list when project is deleted.
+ * likely need a array of array lists than just array of objects.
+ * So that means the array will hold another array inside of it, which will include the task objects
+ * as normal. The array inside the array index will correspond the id so can easily be deleted.
+ *
+ * MAKE A GIT BRANCH TO WORK ON THIS NEW FEATURE AS I WILL HAVE TO REFACTOR ALOT!!!
+ */
